50ef9d17ef2167cb18bdc3da2b647dfbb7709d67
user <user@Mac-mini-administrator-95.local>
2013-09-14 17:16:17 +0400

    dfdf

---
+++ Pedigree/Pedigree/DataFiller.h
@@ -0,0 +1,15 @@
+//
+//  DataFiller.h
+//  Pedigree
+//
+//  Created by user on 14.09.13.
+//  Copyright (c) 2013 user. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataFiller : NSObject
+
++(NSMutableArray *) fillData :(NSArray *)arrayOfIDs and :(NSArray *)arrayOfNames;
+
+@end
---
+++ Pedigree/Pedigree/DataFiller.m
@@ -0,0 +1,13 @@
+//
+//  DataFiller.m
+//  Pedigree
+//
+//  Created by user on 14.09.13.
+//  Copyright (c) 2013 user. All rights reserved.
+//
+
+#import "DataFiller.h"
+
+@implementation DataFiller
+
+@end
